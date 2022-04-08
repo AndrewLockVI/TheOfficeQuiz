e2812cc451ee88e584ebcfca1412f34d6db6af95
AndrewL <adlaack@madisoncollege.edu>
2022-04-08 17:09:31 -0500

    Created Button Listeners

--- app/src/main/java/com/nebuladev/quizgame/MainActivity.kt
+++ app/src/main/java/com/nebuladev/quizgame/MainActivity.kt
@@ -2,10 +2,30 @@ package com.nebuladev.quizgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+
+ private lateinit var buttonOne : Button;
+ private lateinit var buttonTwo : Button;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonOne = findViewById(R.id.answer_one)
+        buttonTwo = findViewById(R.id.answer_two)
+        buttonTwo.setOnClickListener{
+            var text : TextView = findViewById(R.id.txtQuestion)
+            text.text = "Testing button Two"
+        }
+        buttonOne.setOnClickListener{
+            var text : TextView = findViewById(R.id.txtQuestion)
+            text.text = "Testing button One"
+        }
+
     }
 }
