a7374a98c869b5116bfb7872b3d175d8cd7fc68e
AndrewL <adlaack@madisoncollege.edu>
2022-04-19 13:53:37 -0500

    Plugged in values to the end screen and created movement between activities at the end of the game.

--- app/src/main/java/com/nebuladev/quizgame/EndScreen.kt
+++ app/src/main/java/com/nebuladev/quizgame/EndScreen.kt
@@ -1,8 +1,10 @@
 package com.nebuladev.quizgame
 
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.app.NotificationCompat.getExtras
@@ -16,18 +18,57 @@ class EndScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_screen)
+        var levelScore = 100
         var score : String = getIntent().getStringExtra("score").toString()
         var  scoreText : TextView = findViewById(R.id.scoreText)
         var progressBar1 : ProgressBar = findViewById(R.id.progressToLevel)
         scoreText.text = "+" + (score.substringBefore("/").toInt() * 10).toString()
-        progressBar1.progress = scoreText.text.toString().toInt()
         var sharedPreferences : SharedPreferences = getSharedPreferences("score" , MODE_PRIVATE)
         var sharedPreferencesEditor : SharedPreferences.Editor = sharedPreferences.edit()
         sharedPreferencesEditor.putInt("score", sharedPreferences.getInt("score" , 0) + score.substringBefore("/").toInt() * 10)
         sharedPreferencesEditor.commit()
+        if(sharedPreferences.getInt("score", MODE_PRIVATE) >= (sharedPreferences.getInt("level", MODE_PRIVATE)) * levelScore)
+        {
+            sharedPreferencesEditor.putInt("level" , sharedPreferences.getInt("level", MODE_PRIVATE) + 1)
+        }
 
+
+
+        sharedPreferencesEditor.commit()
+        var level : TextView = findViewById(R.id.level)
+        level.text = (sharedPreferences.getInt("level" , MODE_PRIVATE)).toString()
+        var totalScoreInt = sharedPreferences.getInt("score" , MODE_PRIVATE).toString() + " / " + ((sharedPreferences.getInt("level" , MODE_PRIVATE)) * levelScore).toString()
         var totalScore : TextView = findViewById(R.id.totalScore)
-        totalScore.text = sharedPreferences.getInt("score" , 0).toString()
+        totalScore.text = totalScoreInt
+
+        progressBar1.progress = (((sharedPreferences.getInt("score" , MODE_PRIVATE).toDouble() - (sharedPreferences.getInt("level" , MODE_PRIVATE).toDouble() - 1) * levelScore) / levelScore) * 100).toInt()
+
+
+        var mainMenu : Button = findViewById(R.id.mainMenu)
+        mainMenu.setOnClickListener(){
+            var intentMenu : Intent = Intent(this,LandingPage::class.java)
+            startActivity(intentMenu)
+        }
+        var playAgain : Button = findViewById(R.id.playAgain)
+        playAgain.setOnClickListener(){
+            var intentPlay : Intent = Intent(this,MainActivity::class.java)
+            startActivity(intentPlay)
+
+        }
+
+
+
+
+    }
+
+
+    fun resetPrefs()
+    {
+        var sharedPreferences : SharedPreferences = getSharedPreferences("score" , MODE_PRIVATE)
+        var sharedPreferencesEditor : SharedPreferences.Editor = sharedPreferences.edit()
+        sharedPreferencesEditor.putInt("score" , 0)
+        sharedPreferencesEditor.putInt("level" , 0)
+        sharedPreferencesEditor.commit()
     }
 
 
